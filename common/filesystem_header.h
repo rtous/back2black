853c02151ec0cfcb8426e4d084e237997fbaf73b
ruben.tous <rtous@ac.upc.edu>
2024-06-28 13:11:18 +0200

    nc

--- common/filesystem_header.h
+++ common/filesystem_header.h
@@ -7,6 +7,19 @@
 #ifndef __FILESYSTEM_HEADER__
 #define __FILESYSTEM_HEADER__
 
+#if __cplusplus == 202002L
+#	warning "C++20"
+#elif __cplusplus == 201703L
+#    warning "C++17"
+#elif __cplusplus == 201402L
+#    warning "C++14"
+#elif __cplusplus == 201103L
+#    warning "C++11"
+#elif __cplusplus == 199711L
+#    warning "C++98"
+#endif
+
+
 #ifndef __has_include
   static_assert(false, "__has_include not supported");
 #else
@@ -28,5 +41,31 @@
      namespace fs = std::__fs::filesystem;
 #  endif
 #endif
+
+
+/*
+#ifndef __has_include
+  static_assert(false, "__has_include not supported");
+#else
+//#  if __cplusplus >= 201703L && __has_include(<filesystem>)
+//#    warning "fs = std::filesystem;"
+//#    include <filesystem>
+//     namespace fs = std::filesystem;
+//#  elif __has_include(<experimental/filesystem>)
+#  if __has_include(<experimental/filesystem>)
+#    warning "fs = std::experimental::filesystem;"
+#    include <experimental/filesystem>
+     namespace fs = std::experimental::filesystem;
+#  elif __has_include(<boost/filesystem.hpp>)
+#    warning "fs = boost::filesystem;"
+#    include <boost/filesystem.hpp>
+     namespace fs = boost::filesystem;
+#  elif __APPLE__
+#    warning "APPLE detected"
+#    include <filesystem>
+     namespace fs = std::__fs::filesystem;
+#  endif
+#endif
+*/
 
 #endif
--- test_filesystem/main.cpp
+++ test_filesystem/main.cpp
@@ -32,6 +32,20 @@ int main(int argc, char ** argv)
 	std::vector<fs::directory_entry> files_in_directory;
     std::copy(fs::directory_iterator(input_path), fs::directory_iterator(), std::back_inserter(files_in_directory));
     std::sort(files_in_directory.begin(), files_in_directory.end());
+	
+
+    for (const auto & entry : files_in_directory) {
+        std::cout << entry.path() << std::endl;
+		//std::string filename = entry.path().filename();
+		std::string filename = entry.path();
+
+	}
+        /*std::string filename = entry.path().filename();
+        std::string filename_noext = filename.substr(0, filename.find_last_of(".")); 
+
+        if (entry.path().extension() == ".jpg" || entry.path().extension() == ".png") {
+            sam_image_u8 img0;*/
+
 
 
     return 0;
