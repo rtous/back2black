553367a78f80abae372978df32c0b7ec5a026f62
bimailab <ruben.tous@gmail.com>
2024-10-04 16:41:03 +0200

    nc

--- node_addon/initial_study/with-gyp/node-gyp-helloworld-library/build/Release/.deps/Release/addon.node.d
+++
@@ -1,1 +0,0 @@
-cmd_Release/addon.node := c++ -bundle -undefined dynamic_lookup -Wl,-search_paths_first -mmacosx-version-min=10.15 -arch arm64 -L./Release -stdlib=libc++  -o Release/addon.node Release/obj.target/addon/cpphello.o /Users/rtous/dev/back2black/node_addon/with-gyp/node-gyp-helloworld-library/library.a
